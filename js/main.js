da2a8892d1853c3b068dd665ee836e383abbae02
Gabriel Cruz <gabrielcruzb@gmail.com>
2023-02-01 18:40:27 +0100

    create getRandomInt & getSecurePassword functions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 
-
 function getSecurePassword(length) {
 
     const charset = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -16,16 +15,16 @@ function getSecurePassword(length) {
         console.log(charset.length); //72 //72 //72
         console.log(password); //& //&F //&Fc...&FcE@PI7U*db
     }
-    return password;
 }
 
 function getRandomInt(max) {
 
     const randomInt = Math.floor(Math.random() * max) + 8;
-    console.log(getSecurePassword(randomInt));
+    getSecurePassword(randomInt);
 }
 
 getRandomInt(12);
+
 
 //Función getRandomInt que devuelve un número aleatorio entre 0 y 11; Y al resultado le sumamos 8;
 //De esa forma nos aseguramos que el número resultante estará comprendido entre 8 y 19;
